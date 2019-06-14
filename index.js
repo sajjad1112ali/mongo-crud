58b7106f4633ed80e886e3f6312bd1b5b579b84a
sajjad1112ali <sajjadramzan1211@gmail.com>
2019-06-14 17:13:43 +0500

    Added Course insert function

---
+++ index.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/playground")
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch(err => console.error("Could not connect to MongoDB", err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    isPublished: Boolean
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createCourse() {
+    console.log("Creating course");
+
+    const course = new Course({
+        name: "Angular Course",
+        author: "Sajjad",
+        tags: ['angula', 'frontend'],
+        isPublished: true
+    });
+
+    const result = await course.save();
+    console.log(result);
+}
+createCourse();
+
+
+console.log("Exicuting code");
